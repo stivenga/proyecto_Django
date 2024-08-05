093cee9cd42280ba75c838f646f27a927e908573
unknown <stivenguerreroarbelaez@gmail.com>
2024-08-04 19:38:03 -0500

    ultimo commit

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Tasks
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Tasks
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'write a description'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input '}),
+        }
